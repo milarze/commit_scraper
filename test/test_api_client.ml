b6348f3faa85a3b7dcfdde0d0b07db6918a0cb3f
Ezra Lim <ezralim1992@gmail.com>
2025-05-18 13:25:24 +0800

    refactor: API Client
    Make the API Client more easily testable by other modules

--- test/test_api_client.ml
+++ test/test_api_client.ml
@@ -1,7 +1,7 @@
 open Alcotest
 open Commit_scraper.Api_client
 
-let test_get_url () =
+let test_list_commits_url () =
   check string "username/reponame"
     "https://api.github.com/repos/username/reponame/commits"
     (list_commits_url ~repo:"username/reponame");
@@ -12,7 +12,7 @@ let test_get_url () =
     "https://api.github.com/repos/user_name/repo_name/commits"
     (list_commits_url ~repo:"user_name/repo_name")
 
-let test_get_url_invalid () =
+let test_list_commits_url_invalid () =
   check_raises "Invalid repository format: invalid-repo"
     (Failure "Invalid repository format: invalid-repo. Expected format: username/reponame")
     (fun () -> ignore (list_commits_url ~repo:"invalid-repo"));
@@ -25,10 +25,60 @@ let test_get_url_invalid () =
        "Invalid repository format: invalid_repo_name. Expected format: username/reponame")
     (fun () -> ignore (list_commits_url ~repo:"invalid_repo_name"))
 
+let test_valid_repo_name () =
+  check bool "Valid repository name" true (is_valid_repo_name ~repo:"username/reponame");
+  check bool "Valid repository name" true (is_valid_repo_name ~repo:"user-name/repo-name");
+  check bool "Valid repository name" true (is_valid_repo_name ~repo:"user_name/repo_name");
+  check bool "Valid repository name" true (is_valid_repo_name ~repo:"user.name/repo.name");
+  check bool "Valid repository name" true (is_valid_repo_name ~repo:"user-name/repo.name");
+  check bool "Valid repository name" true (is_valid_repo_name ~repo:"user.name/repo-name");
+  check bool "Invalid repository name" false (is_valid_repo_name ~repo:"invalid-repo");
+  check bool "Invalid repository name" false
+    (is_valid_repo_name ~repo:"invalid/repo/name");
+  check bool "Invalid repository name" false
+    (is_valid_repo_name ~repo:"invalid_repo_name")
+
+let test_extract_next_page_url () =
+  (let headers = Cohttp.Header.init () in
+   let headers =
+     Cohttp.Header.add headers "Link" "<https://api.github.com/next>; rel=\"next\""
+   in
+   match extract_next_page_url ~headers with
+   | Some url -> check string "Next page URL" "https://api.github.com/next" url
+   | None -> failwith "No next page URL found");
+  (let headers = Cohttp.Header.init () in
+   let headers =
+     Cohttp.Header.add headers "Link" "<https://api.github.com/prev>; rel=\"prev\""
+   in
+   match extract_next_page_url ~headers with
+   | Some _ -> failwith "Expected no next page URL, but got one"
+   | None -> check string "No next page URL" "None" "None");
+  (let headers = Cohttp.Header.init () in
+   let headers =
+     Cohttp.Header.add headers "Link"
+       "<https://api.github.com/next>; rel=\"next\", <https://api.github.com/prev>; \
+        rel=\"prev\""
+   in
+   match extract_next_page_url ~headers with
+   | Some url -> check string "Next page URL" "https://api.github.com/next" url
+   | None -> failwith "No next page URL found");
+  let headers = Cohttp.Header.init () in
+  let headers =
+    Cohttp.Header.add headers "Link" "https://api.github.com/next; rel=\"next\""
+  in
+  match extract_next_page_url ~headers with
+  | Some _ -> failwith "Expected no next page URL, but got one"
+  | None -> check string "No next page URL" "None" "None"
+
 let () =
   run "ApiClient"
     [
-      ("test_get_url", [ test_case "Valid repository" `Quick test_get_url ]);
-      ( "test_get_url_invalid",
-        [ test_case "Invalid repository" `Quick test_get_url_invalid ] );
+      ( "test_list_commits_url",
+        [ test_case "Valid list commits url" `Quick test_list_commits_url ] );
+      ( "test_list_commits_url_invalid",
+        [ test_case "Invalid repository" `Quick test_list_commits_url_invalid ] );
+      ( "test_valid_repo_name",
+        [ test_case "Valid repository name" `Quick test_valid_repo_name ] );
+      ( "test_extract_next_page_url",
+        [ test_case "Extract next page URL" `Quick test_extract_next_page_url ] );
     ]
