95a0fc1f8b314724cfce1fa9fe50f438d6c5f273
Ezra Lim <ezralim1992@gmail.com>
2025-05-26 00:12:14 +0800

    refactor: Extract repo name validation into util

---
+++ lib/utils.mli
@@ -0,0 +1,3 @@
+val is_valid_repo_name : repo:string -> bool
+(** [is_valid_repo_name ~repo] checks if the given repository name is valid. A valid
+    repository name should be in the format "owner/repo". *)
